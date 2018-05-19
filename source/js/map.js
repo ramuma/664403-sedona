0b406e0144cead19afc35f13a38c9e0a967c8d78
ramuma <merlya@yandex.ru>
2018-05-19 17:18:42 +0400

    Поправила путь к иконке маркера

--- source/js/map.js
+++ source/js/map.js
@@ -5,7 +5,7 @@ function initialize() {
     }
     var map = new google.maps.Map(document.getElementById('map-canvas'),
                                   mapOptions);
-    var image = "../img/icon-map-marker.svg";
+    var image = "img/icon-map-marker.svg";
     var myLatLng = new google.maps.LatLng(34.8544438,-111.830158,13);
     var beachMarker = new google.maps.Marker({
       position: myLatLng,
