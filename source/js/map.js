ce1f67907af6f8e139064e506dd10fb697aa2e94
ramuma <merlya@yandex.ru>
2018-05-23 22:17:55 +0400

    PP

--- source/js/map.js
+++ source/js/map.js
@@ -1,12 +1,12 @@
 function initialize() {
     var mapOptions = {
       zoom: 7,
-      center: new google.maps.LatLng(34.8544438,-111.830158,13)
+      center: new google.maps.LatLng(34.8545802,-111.9348809,11)
     }
     var map = new google.maps.Map(document.getElementById('map-canvas'),
                                   mapOptions);
     var image = "img/icon-map-marker.svg";
-    var myLatLng = new google.maps.LatLng(34.8544438,-111.830158,13);
+    var myLatLng = new google.maps.LatLng(34.7244438,-111.830158,13);
     var beachMarker = new google.maps.Marker({
       position: myLatLng,
       map: map,
