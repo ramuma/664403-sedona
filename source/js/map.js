87a3cb3777df3afd7c9aec11ebaefadf686a7ea4
ramuma <merlya@yandex.ru>
2018-05-25 17:03:58 +0400

    Поменяла img на svg в intro

--- source/js/map.js
+++ source/js/map.js
@@ -3,7 +3,7 @@ function initialize() {
     zoom: 7,
     center: new google.maps.LatLng(34.8545802, -111.9348809, 11)
   }
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
     mapOptions);
   var image = "img/icon-map-marker.svg";
   var myLatLng = new google.maps.LatLng(34.7244438, -111.830158, 13);
